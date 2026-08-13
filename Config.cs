77178e9c102b7bd1e7032e1496089f087b7d66f4
Jinjyu1211 <1157719728@qq.com>
2026-08-13 13:59:38 +0800

    v0.3.0: add VFX drawing (donut rings)

--- Config.cs
+++ Config.cs
@@ -3,12 +3,21 @@ using Dalamud.Configuration;
 
 namespace TargetRangeRing;
 
+public enum RenderBackend
+{
+    ImGui,
+    DirectX,
+    VFX,
+}
+
 public sealed class Config : IPluginConfiguration
 {
-    public int Version { get; set; } = 9;
+    public int Version { get; set; } = 11;
 
     public bool Enabled { get; set; } = true;
     public float Thickness { get; set; } = 1f;
+    public float VfxThickness { get; set; } = 0.4f;
+    public RenderBackend Backend { get; set; } = RenderBackend.ImGui;
 
     public Vector4 AutoAttackColor { get; set; } = new(1f, 0.8f, 0f, 0.8f);
     public Vector4 MaxAttackColor { get; set; } = new(0.3f, 0.7f, 0.3f, 0.8f);
